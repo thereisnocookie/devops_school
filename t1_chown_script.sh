5586c45527dc5f2a9015ebfbc9a071d5fec52b0f
Taras <sephiroth.priest@gmail.com>
2021-11-20 14:29:14 +0200

    some script improvemets

--- t1_chown_script.sh
+++ t1_chown_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ id "$1" &>/dev/null ] || [ -d $2 ]; then
-	chown -R $1:$1 $2
+	chown -R $1:$1 $2 && echo "Done!"
 else
+	echo "Please use syntax: ./script.sh <usermame> <foldername>"
 	echo "$2 is not a directory or not exist"
 fi
